234909ceb923906c5262bff42a4091837722ecc9
Minsoo Kim <minsooerickim@gmail.com>
2023-01-15 00:57:00 -0800

    add navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import Navbar from '@/components/Navbar'
 import ThemeButton from '@/components/ThemeButton'
 import { ThemeProvider } from 'next-themes'
 import type { AppProps } from 'next/app'
@@ -6,7 +7,8 @@ import '../styles/globals.css'
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
-      <ThemeButton />
+      {/* <ThemeButton /> */}
+      <Navbar />
       <Component {...pageProps} />
     </ThemeProvider>
   )
