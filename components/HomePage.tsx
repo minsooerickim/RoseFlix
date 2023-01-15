0a664cfbd33c1486766e5a63b9c30ac0b4696dc7
Roy Feng <roy.feiyang.feng@gmail.com>
2023-01-15 05:54:14 -0800

    home page final touches

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -16,28 +16,20 @@ export default function HomePage() {
         </div>
         <div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
           <h1 className="text-5xl font-bold leading-none sm:text-6xl">
-            RoseFlicks
-            <span className="dark:text-indigo-400">weoi</span>erat pharetra
+            Rose
+            <span className="dark:text-indigo-400">Flix</span>
           </h1>
           <p className="mt-6 mb-8 text-lg sm:mb-12">
-            Dictum aliquam porta in condimentum ac integer
+          A Netflix-like software for personalized recommendations for women-centric content using machine learning and prioritizing diversity and inclusivity
             <br className="hidden md:inline lg:hidden" />
-            turpis pulvinar, est scelerisque ligula sem
           </p>
           <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
             <a
               rel="noopener noreferrer"
-              href="#"
+              href="search"
               className="px-8 py-3 text-lg font-semibold rounded dark:bg-indigo-400 dark:text-gray-900"
             >
-              Suspendisse
-            </a>
-            <a
-              rel="noopener noreferrer"
-              href="#"
-              className="px-8 py-3 text-lg font-semibold border rounded dark:border-gray-100"
-            >
-              Malesuada
+              Search Now
             </a>
           </div>
         </div>
